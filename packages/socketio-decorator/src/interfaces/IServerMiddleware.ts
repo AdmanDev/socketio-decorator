57c7370f5722f54bca62fdaa250f28c8f76a2092
ADMAN <adman.software@gmail.com>
2024-07-24 21:28:56 +0200

    chore: Add middleware function documentation for socket and server middleware interface

--- packages/socketio-decorator/src/interfaces/IServerMiddleware.ts
+++ packages/socketio-decorator/src/interfaces/IServerMiddleware.ts
@@ -4,5 +4,10 @@ import { Socket } from "socket.io"
  * Defines the server middleware interface
  */
 export type IServerMiddleware = {
+	/**
+	 * The middleware function to use
+	 * @param {Socket} socket The socket object
+	 * @param {Function} next The next function to call
+	 */
 	use: (socket: Socket, next: (err?: Any) => void) => void
 }
