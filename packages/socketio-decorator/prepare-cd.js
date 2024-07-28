8d63f43cdad38d7c5e0fd6ad0b24d9c2d0a92724
ADMAN <adman.software@gmail.com>
2024-07-28 17:18:28 +0200

    cd pipeline

---
+++ packages/socketio-decorator/prepare-cd.js
@@ -0,0 +1,23 @@
+const shelljs = require("shelljs")
+const pkg = require("./package.json")
+
+// ---------------------------------------- Copy package.json ---------------------------------------- //
+
+delete pkg.devDependencies
+delete pkg.scripts
+delete pkg.files
+
+// Replace "dist" with "."
+let pkgJson = JSON.stringify(pkg, null, 2)
+
+pkgJson = pkgJson.replace(/"\.\/dist\//g, "\"./")
+pkgJson = pkgJson.replace(/"dist\//g, "\"./")
+
+shelljs.mkdir("-p", "dist")
+
+shelljs.ShellString(pkgJson).to("dist/package.json")
+
+// ---------------------------------------- Copy files ---------------------------------------- //
+
+shelljs.cp("../../README.md", "dist/README.md")
+shelljs.cp("../../LICENSE", "dist/LICENSE")
