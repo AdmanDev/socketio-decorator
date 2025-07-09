35372443ffae0f4821bd1463813ba68425350efb
ADMAN <adman.software@gmail.com>
2025-07-09 11:24:55 +0200

    refactor: remove deprecated useCurrentUser hook

--- packages/socketio-decorator/src/Others/Hooks.ts
+++ packages/socketio-decorator/src/Others/Hooks.ts
@@ -10,21 +10,6 @@ export function useIoServer (): Server {
 }
 
 /**
- * Get the current user from the socket instance
- * @deprecated This hook is deprecated and will be removed in future versions. Use `@CurrentUser()` decorator instead.
- * @param {Socket} socket The socket instance
- * @returns {Promise<TUser | null>} The current user
- * @template TUser The user type
- */
-export async function useCurrentUser<TUser> (socket: Socket) {
-	if (!config.currentUserProvider) { // TODO: remove this hook
-		return Promise.resolve(null)
-	}
-
-	return await config.currentUserProvider(socket) as TUser | null
-}
-
-/**
  * Get the socket instance from some argument
  * @param {T} arg The argument to search the socket
  * @template T The argument type
